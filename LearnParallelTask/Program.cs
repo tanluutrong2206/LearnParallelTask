e21d2a3e763e0f99e180c2484848f1d3df4fb44f
Lưu Trọng Tấn <tanluutrong2206@gmail.com>
2022-04-18 00:09:45 +0700

    Add project files.

---
+++ LearnParallelTask/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var cts = new CancellationTokenSource();
+var t = new Task(() =>
+{
+    int i = 0;
+    while (true)
+    {
+        cts.Token.ThrowIfCancellationRequested();
+        Console.WriteLine(i++);
+    }
+}, cts.Token);
+t.Start();
+Console.ReadKey();
+
+cts.Cancel();
